dff06d67fbd791ed728a4fafc5b2365961e99270
HuaGuan <G18174078632@gmail.com>
2022-03-12 14:33:58 +0800

    设置沉浸式状态栏，白底黑字

---
+++ app/src/main/java/pri/guanhua/myemoji/ContentFragment.java
@@ -0,0 +1,24 @@
+package pri.guanhua.myemoji;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ContentFragment extends Fragment {
+
+    private View mView = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (mView == null){
+            mView = inflater.inflate(R.layout.fragment_content, container, false);
+        }
+        return mView;
+    }
+}
--- app/src/main/java/pri/guanhua/myemoji/MainActivity.java
+++ app/src/main/java/pri/guanhua/myemoji/MainActivity.java
@@ -1,13 +1,86 @@
 package pri.guanhua.myemoji;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ContentFragment mContentFragment = null;
+    private FrameLayout mContainer = null;
+    //fragment管理器
+    private FragmentManager mSupportFragmentManager = null;
+    //Toolbar
+    private Toolbar mToolbar = null;
+    //抽屉容器
+    private DrawerLayout mDrawerLayout = null;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setToolbar();
+        setDrawerLayout();
+        setContentFragment();
     }
+
+    private void initView(){
+        if (mContainer == null) {
+            mContainer = findViewById(R.id.content_frame);
+        }
+        if (mToolbar == null){
+            mToolbar = findViewById(R.id.toolbar);
+        }
+        if (mDrawerLayout == null){
+            mDrawerLayout = findViewById(R.id.drawer_layout);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void setToolbar(){
+        setSupportActionBar(mToolbar);
+        //Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeButtonEnabled(true);
+        //把状态栏的文字设置为黑色
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        //把状态栏设置透明
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+
+    private void setDrawerLayout(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                mToolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+        toggle.syncState();
+        mDrawerLayout.addDrawerListener(toggle);
+    }
+
+    private void setContentFragment(){
+        if (mContentFragment == null){
+            mContentFragment = new ContentFragment();
+        }
+        if (mSupportFragmentManager == null){
+            mSupportFragmentManager = getSupportFragmentManager();
+        }
+        FragmentTransaction fragmentTransaction = mSupportFragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content_frame, mContentFragment).commit();
+    }
+
 }

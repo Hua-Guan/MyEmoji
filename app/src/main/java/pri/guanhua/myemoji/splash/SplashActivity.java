4975ce14b65df4ca8de995fdaaeddf61a8defc7c
HuaGuan <G18174078632@gmail.com>
2022-03-21 23:49:55 +0800

    增加了splash页面

---
+++ app/src/main/java/pri/guanhua/myemoji/splash/SplashActivity.java
@@ -0,0 +1,27 @@
+package pri.guanhua.myemoji.splash;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import pri.guanhua.myemoji.MainActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
--- app/src/main/java/pri/guanhua/myemoji/view/login/LoginActivity.java
+++ app/src/main/java/pri/guanhua/myemoji/view/login/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -13,12 +14,16 @@ import pri.guanhua.myemoji.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ImageView mBack = null;
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setStatusBar();
+        initView();
+        setBack();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -27,6 +32,19 @@ public class LoginActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         //把状态栏设置透明
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+
+    private void initView(){
+        mBack = findViewById(R.id.img_back);
+    }
+
+    private void setBack(){
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
